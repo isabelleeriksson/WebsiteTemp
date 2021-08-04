8b38727e390786014776a97fac87399c16486cd3
swaggychiken <88410992+swaggychiken@users.noreply.github.com>
2021-08-03 18:13:56 -0700

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+      '_id': '1',
+      'name': 'Dog Dragon Hybrid',
+      'image': '/adoptimages/dogdragon.jpg',
+      'description':'A simple design of a dog and dragon hybrid. Jacket design is included with purchase.',
+      'price': 49.99,
+      'category': 'adopt',
+      'countInStock': 0,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+        '_id': '2',
+        'name': 'Flowery Racoon',
+        'image': '/adoptimages/racoon.jpg',
+        'description':'A cute racoon with a white flowered jacket.',
+        'price': 24.99,
+        'category': 'adopt',
+        'countInStock': 1,
+        'rating': 3.0,
+        'numReviews': 8,
+      },
+      {
+        '_id': '3',
+        'name': 'Traditional Inspired Dragon',
+        'image': '/adoptimages/dragon.jpg',
+        'description':'A beautiful design of a white and blue dragon. A cherry blossomed oil-paper umbrella and traditional chinese ornaments.',
+        'price': 59.99,
+        'category': 'adopt',
+        'countInStock': 0,
+        'rating': 4.8,
+        'numReviews': 12,
+      },
+      {
+        '_id': '4',
+        'name': 'Dog Shark Hybrid',
+        'image': '/adoptimages/dogshark.jpg',
+        'description':'A white fluffy dog and shark hybrid. ',
+        'price': 59.99,
+        'category': 'adopt',
+        'countInStock': 1,
+        'rating': 4.5,
+        'numReviews': 10,
+      },
+  ]
+  
+  
+  export default products
+  
